904867075e03a0da0c4ecbd2af3f62010dc8d9b8
njnaghamanickam <njnaghamanickam@gmail.com>
2020-09-18 11:03:04 +0530

    branch A code changed

--- src/test/java/org/project/NewProject.java
+++ src/test/java/org/project/NewProject.java
@@ -4,7 +4,9 @@ package org.project;
 public class NewProject {
 	
 	public static void main(String[] args) {
-		System.out.println("Code updated to upload in the branch");
+		System.out.println("Code changed in Branch A");
+		
+		
 		
 			}
 	
